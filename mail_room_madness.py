be2d75bda6e52fb5d141144123f2c8be04236e8c
Brendan Davis <brendanmd@gmail.com>
2017-10-20 15:57:06 -0700

    working on the letter

--- mail_room_madness.py
+++ mail_room_madness.py
@@ -1,9 +1,11 @@
+"""Process a donation & issue a thankyou letter or generate a report."""
+
 DONORS_AMT = {'example': 100, 'other': 20, 'more': 200}
-DONORS_CT = {'example': 1, 'other': 2, 'more':}
+DONORS_CT = {'example': 1, 'other': 2, 'more': 3}
 
 
 def mail_room():
-    """Menu Interface"""
+    """Menu Interface."""
     reply = None
     while reply is not quit:
         print("Message")
@@ -11,7 +13,7 @@ def mail_room():
         print("2: Create a Report")
         print("3. Quit this program")
         print("(Enter 'q' at any time to return to this menu.)")
-        reply = int(input("1 or 2?"))  #Input
+        reply = int(input("1 or 2?"))  # Input
         if reply == 1:
             add_amount()
         if reply == 2:
@@ -20,34 +22,44 @@ def mail_room():
             reply = quit
 
 
-def add_amount(): #Assert that DONOR is now in DONORS_AMT?
-    """Adds the amount to the donor's total"""
+def add_amount():  # Assert that DONOR is now in DONORS_AMT?
+    """Add the amount to the donor's total."""
     amount = ""
-    name = input("Enter the donor's name: ")  #Input
+    name = input("Enter the donor's name: ")  # Input
     if name == "q":
         return
     while not amount.isdigit():
-        amount = input("Enter their donation: ")  #Input
+        amount = input("Enter their donation: ")  # Input
         if amount == "q":
             return
     if name in DONORS_AMT:
-        DONORS_AMT[name] += amount
-        donors_ct[name] += 1
+        DONORS_AMT[name] += int(amount)
+        DONORS_CT[name] += 1
     else:
-        DONORS_AMT[name] = amount
-        donors_ct[name] = 1
-    #print_letter()
+        DONORS_AMT[name] = int(amount)
+        DONORS_CT[name] = 1
+    print_letter()
 
 
 def report():
-    """This will print out the report"""   
-    print("\tName\t\tTimes\tAmount\tAverage")
-    #for donor in DONORS_AMT:
-        #print("\t{}\t\t{}\t${}\t${}".format(donor, donors_ct[donor], DONORS_AMT[donor], DONORS_AMT[donor]/donors_ct[donor] ))
-    s = [(k, d[k]) for k in sorted(DONORS_AMT, key=d.get)]
-#def print_letter():
-    
-    #This is where we'll print the letter
+    """Print the donor report."""
+    print("\tName\t\tDonation\tTotal Amt\tAverage")
+    x = sorted(DONORS_AMT, key=DONORS_AMT.get)
+    for donor in x:
+        print("\t{}\t\t{}\t\t${}\t\t${}".format(
+            donor,
+            DONORS_CT[donor],
+            DONORS_AMT[donor],
+            DONORS_AMT[donor] / DONORS_CT[donor]))
 
+
+def print_letter(to, contribution):
+    """Print the thank you letter to the donor."""
+    print("""Dear Mr. or Mrs. {},
+    \nThank you very much for your generous donation of ${}. It's thanks
+     to people like you that we are able to continue our noble cause of
+     shaving cats. Without your generous contribution even more cats
+     would be living on the streets with a full coat of hair""".format(
+        to, contribution))
 
 mail_room()
